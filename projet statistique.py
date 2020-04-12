6b3fbc9583f781ad9e7459953c2ccacc8795350f
khoudiaMbodji <titimbodji@gmail.com>
2020-04-12 05:06:08 +0200

    titi

---
+++ projet statistique.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 16:06:42 2020
+
+@author: khoudia Mbodj
+"""
+
+###KHOUDIA MBODJI.QUESTION 1:TROISIEMENT:SIMULATION DELA DENSITé DE LOI GAMMA
+import numpy as np
+import matplotlib.pyplot as plt
+# Nombre de valeurs à générer
+n = 10000
+a=10
+### Génération des données
+data = np.random.gamma(2,1, length)
+plt.figure(figsize=(12,8))
+plt.plot(x, pdf, label=dist_name, linewidth=5) 
+plt.plot(x, y, alpha=0.5)
+plt.legend()
+plt.show()
